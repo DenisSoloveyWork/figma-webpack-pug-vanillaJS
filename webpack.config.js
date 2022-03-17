489be50a5db02dda111707357a15e59ba8b5b727
NeutralWinter <neutralvvinter@gmail.com>
2022-03-17 22:21:46 +0300

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,24 @@ const InlineChunkHtmlPlugin = require('inline-chunk-html-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   path = require('path');
 
-module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+let plugins = [
+  new HtmlWebpackPlugin({
+    template: './app/pug/index.pug',
+    filename: 'ui.html',
+    inject: 'body',
+    chunks: ['ui'],
+  }),
+];
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  plugins.push(new InlineChunkHtmlPlugin(HtmlWebpackPlugin, ['.js$']))
+}
+
+module.exports = {
+  mode: mode,
+  devtool: mode === 'production' ? false : 'inline-source-map',
   entry: {
     ui: './app/js/ui/ui.js',
     code: './app/js/code/code.js',
@@ -25,13 +40,5 @@ module.exports = (env, argv) => ({
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './app/pug/index.pug',
-      filename: 'ui.html',
-      inject: 'body',
-      chunks: ['ui'],
-    }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, ['.js$']),
-  ],
-});
+  plugins: plugins,
+};
