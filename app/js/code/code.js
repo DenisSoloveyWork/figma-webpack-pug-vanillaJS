f32f3e86468ab0fc4cfdca772c26d485652133ce
DenSolovey <denis.solovey.design@gmail.com>
2023-05-18 13:53:25 +0300

    commit

--- app/js/code/code.js
+++ app/js/code/code.js
@@ -1,6 +1,6 @@
 //// ================================ Imports ======================================
 
 import {} from '@figma-plugin/helpers' // - https://github.com/figma-plugin-helper-functions/figma-plugin-helpers
-
+import {} from '@create-figma-plugin/utilities' // - https://yuanqing.github.io/create-figma-plugin/utilities/
 
 //// ================================ Code ======================================
